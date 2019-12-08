4f88a8877c20cf3da139801cdbd826042b77d36e
Nazar <nazar.rybchuk1999@gmail.com>
2019-12-08 13:21:03 +0300

    Modify js file

--- script.js
+++ script.js
@@ -12,13 +12,44 @@ let appData = {
     savings: false
 };
 console.log(appData);
+/*
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
-    b1 = prompt("Во сколько обойдется?"),
-    a2 = prompt("Введите обязательную статью расходов в этом месяце"),
-    b2 = prompt("Во сколько обойдется?");
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+        a != '' && b != '' && a.length < 50) {
+        console.log('Done!');
+        appData.expenses[a] = b;
+    } else {
+        i--; // Цыкл начинаеться сначала
+    }
+};
 
-    appData.expenses[a1] = b1;
-    appData.expenses[a2] = b2;
-    console.log(appData);
- console.log(alert(appData.budget/30));
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошыбка!");
+}
+*/
+let i = 0;
+while(i<2){
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
+        i++;
+        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+        a != '' && b != '' && a.length < 50){ 
+        console.log('Done!');
+        appData.expenses[a] = b;
+        } else{
+            i =0;
+        }
+        
+}
